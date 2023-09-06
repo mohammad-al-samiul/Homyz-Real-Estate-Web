b5f56526da9bbcfae119bcedd2a8f2a600b4a56e
Noob-H4cker <alsamiul123@gmail.com>
2023-09-06 21:43:05 +0600

    resposive menu category

--- src/Components/Cover/Cover.js
+++ src/Components/Cover/Cover.js
@@ -5,12 +5,8 @@ import { Parallax } from 'react-parallax';
 const Cover = ({ menuImage, title }) => {
   return (
     <div>
-      <Parallax
-        blur={{ min: -15, max: 15 }}
-        bgImage={menuImage}
-        bgImageAlt="the dog"
-        strength={-200}>
-        <div className=" hero h-[600px] text-white">
+      <Parallax blur={{ min: -15, max: 15 }} bgImage={menuImage} bgImageAlt="" strength={-200}>
+        <div className=" hero lg:h-[600px] text-white">
           <div className="hero-content text-center">
             <div className="max-w-md">
               <h1 className="text-5xl font-bold">{title}</h1>
