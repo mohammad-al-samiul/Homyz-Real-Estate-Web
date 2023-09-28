225f1b4ffd032abbbb8432ee778cd0f849538abc
Noob-H4cker <alsamiul123@gmail.com>
2023-09-28 10:32:12 +0600

    make admin

--- src/Layouts/DashboardLayout/DashboardLayout.js
+++ src/Layouts/DashboardLayout/DashboardLayout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 import {
   FaCalendarAlt,
   FaHamburger,
@@ -15,84 +16,89 @@ const DashboardLayout = () => {
   const isAdmin = true;
 
   return (
-    <div className="drawer lg:drawer-open">
-      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content">
-        <Outlet />
+    <div>
+      <Helmet>
+        <title>Regal Dragon | Dashboard</title>
+      </Helmet>
+      <div className="drawer lg:drawer-open">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          <Outlet />
 
-        <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">
-          Open drawer
-        </label>
-      </div>
-      <div className="drawer-side">
-        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-        <ul className="menu p-4 w-80 min-h-full  text-base-content bg-[#D1A054]">
-          {/* Sidebar content here */}
-          {isAdmin ? (
-            <>
-              <li>
-                <NavLink to={'/dashboard/admin-home'}>
-                  <FaHome></FaHome> <span className="mt-1">Admin Home</span>
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to={'/dashboard/add-item'}>
-                  <FaUtensils></FaUtensils> <span className="mt-1">Add Item</span>
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to={'/dashboard/manage-item'}>
-                  <FaHamburger></FaHamburger> <span className="mt-1">Managd Items</span>
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to={'/dashboard/all-users'}>
-                  <FaUsers></FaUsers> <span className="mt-1"> All Users</span>
-                </NavLink>
-              </li>
-            </>
-          ) : (
-            <>
-              <li>
-                <NavLink to={'/user-home'}>
-                  <FaHome></FaHome> <span className="mt-1">User Home</span>
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to={'/reservation'}>
-                  <FaCalendarAlt></FaCalendarAlt> <span className="mt-1">Reservation</span>
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to={'/payment'}>
-                  <FaWallet></FaWallet> <span className="mt-1">Payment History</span>
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to={'/dashboard/my-cart'}>
-                  <FaShoppingCart></FaShoppingCart> <span className="mt-1"> My Cart</span>
-                </NavLink>
-              </li>
-            </>
-          )}
+          <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">
+            Open drawer
+          </label>
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 min-h-full  text-base-content bg-[#D1A054]">
+            {/* Sidebar content here */}
+            {isAdmin ? (
+              <>
+                <li>
+                  <NavLink to={'/dashboard/admin-home'}>
+                    <FaHome></FaHome> <span className="mt-1">Admin Home</span>
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to={'/dashboard/add-item'}>
+                    <FaUtensils></FaUtensils> <span className="mt-1">Add Item</span>
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to={'/dashboard/manage-item'}>
+                    <FaHamburger></FaHamburger> <span className="mt-1">Managd Items</span>
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to={'/dashboard/all-users'}>
+                    <FaUsers></FaUsers> <span className="mt-1"> All Users</span>
+                  </NavLink>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <NavLink to={'/user-home'}>
+                    <FaHome></FaHome> <span className="mt-1">User Home</span>
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to={'/reservation'}>
+                    <FaCalendarAlt></FaCalendarAlt> <span className="mt-1">Reservation</span>
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to={'/payment'}>
+                    <FaWallet></FaWallet> <span className="mt-1">Payment History</span>
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to={'/dashboard/my-cart'}>
+                    <FaShoppingCart></FaShoppingCart> <span className="mt-1"> My Cart</span>
+                  </NavLink>
+                </li>
+              </>
+            )}
 
-          <div className="divider"></div>
-          <li>
-            <NavLink to={'/home'}>
-              <FaHome></FaHome> <span className="mt-1"> Home</span>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={'/home'}>
-              <FaHamburger></FaHamburger> <span className="mt-1"> Our Menu</span>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={'/home'}>
-              <FaShopify></FaShopify> <span className="mt-1"> Order Food</span>
-            </NavLink>
-          </li>
-        </ul>
+            <div className="divider"></div>
+            <li>
+              <NavLink to={'/home'}>
+                <FaHome></FaHome> <span className="mt-1"> Home</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={'/home'}>
+                <FaHamburger></FaHamburger> <span className="mt-1"> Our Menu</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={'/home'}>
+                <FaShopify></FaShopify> <span className="mt-1"> Order Food</span>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/Pages/Dashboard/AllUsers/AllUsers.js
+++ src/Pages/Dashboard/AllUsers/AllUsers.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { FaTrashAlt } from 'react-icons/fa';
 import { FaUserShield } from 'react-icons/fa6';
+import Swal from 'sweetalert2';
 
 const AllUsers = () => {
   const { refetch, data: users = [] } = useQuery({
@@ -11,63 +12,76 @@ const AllUsers = () => {
     queryFn: () => fetch(`http://localhost:5000/users`).then((res) => res.json())
   });
 
-  const handleMakeAdmin = (id) => {
-    console.log(id);
+  const handleMakeAdmin = (user) => {
+    console.log(user);
+    fetch(`http://localhost:5000/user/admin/${user?._id}`, {
+      method: 'PATCH'
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount) {
+          Swal.fire('Good job!', `${user?.name}  is admin now!`, 'success');
+        }
+      });
   };
 
   const handleDelete = (id) => {
     console.log(id);
   };
   return (
-    <div className="lg:w-full p-10 ">
+    <div>
       <Helmet>
         <title>Regal Dragon | All Users</title>
       </Helmet>
-      <h3 className="text-2xl font-bold text-center">Total Users : {users?.length}</h3>
-      <div className="my-4 overflow-x-auto shadow-lg rounded">
-        <table className="table">
-          {/* head */}
-          <thead>
-            <tr>
-              <th></th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Role</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users?.map((user, index) => (
-              <tr key={user._id} className="hover">
-                <th>{index + 1}</th>
-                <td>{user?.name}</td>
-                <td>{user?.email}</td>
-                <td>
-                  {' '}
-                  {user?.role == 'admin' ? (
-                    'admin'
-                  ) : (
-                    <>
-                      <button
-                        onClick={() => handleMakeAdmin(user?._id)}
-                        className="text-white bg-orange-400 btn hover:bg-orange-400 rounded">
-                        <FaUserShield></FaUserShield>
-                      </button>
-                    </>
-                  )}
-                </td>
-                <td>
-                  <button
-                    onClick={() => handleDelete(user?._id)}
-                    className="btn hover:bg-red-600 text-white bg-red-600 rounded">
-                    <FaTrashAlt></FaTrashAlt>
-                  </button>
-                </td>
+      <div className="lg:w-full p-10 ">
+        <h3 className="text-2xl font-bold text-center">Total Users : {users?.length}</h3>
+        <div className="my-4 overflow-x-auto shadow-lg rounded">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Role</th>
+                <th>Action</th>
               </tr>
-            ))}
-            {/* row 2 */}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {users?.map((user, index) => (
+                <tr key={user._id} className="hover">
+                  <th>{index + 1}</th>
+                  <td>{user?.name}</td>
+                  <td>{user?.email}</td>
+                  <td>
+                    {' '}
+                    {user?.role == 'admin' ? (
+                      'admin'
+                    ) : (
+                      <>
+                        <button
+                          onClick={() => {
+                            handleMakeAdmin(user);
+                          }}
+                          className="text-white bg-orange-400 btn hover:bg-orange-400 rounded">
+                          <FaUserShield></FaUserShield>
+                        </button>
+                      </>
+                    )}
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => handleDelete(user)}
+                      className="btn hover:bg-red-600 text-white bg-red-600 rounded">
+                      <FaTrashAlt></FaTrashAlt>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+              {/* row 2 */}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
