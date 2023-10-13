4b25f53d4d3e3c71668a61c44259e07648febcc7
Noob-H4cker <alsamiul123@gmail.com>
2023-10-14 00:11:18 +0600

    form validation

--- src/Pages/Dashboard/AddItem/AddItem.js
+++ src/Pages/Dashboard/AddItem/AddItem.js
@@ -17,26 +17,27 @@ const AddItem = () => {
     <div className="">
       <div>
         <SectionTitle subHeading={`What's New ?`} heading={`Add an Item`}></SectionTitle>
-        <div className="mx-5 bg-[#F3F3F3] p-5">
+        <div className="mx-5 bg-[#F3F3F3] p-10 rounded-lg">
           <form onSubmit={handleSubmit(onSubmit)}>
             <div className="form-control w-full ">
               <label className="label">
                 <span className="label-text font-bold">Recipe Name</span>
               </label>
               <input
-                {...register('recipeName')}
+                {...register('recipeName', { required: 'Recipe Name is required' })}
                 type="text"
                 placeholder="Recipe Name"
                 className="input input-bordered w-full "
               />
             </div>
+            {errors.recipeName && <p className="text-red-600">{errors.recipeName.message}</p>}
             <div className="flex">
               <div className="form-control w-full mr-2">
                 <label className="label">
                   <span className="label-text font-bold">Category</span>
                 </label>
                 <select
-                  {...register('category', { required: true })}
+                  {...register('category', { required: 'Category is required' })}
                   className="select select-bordered">
                   <option disabled>Pick one</option>
                   <option>Salad</option>
@@ -46,30 +47,44 @@ const AddItem = () => {
                   <option>Drinks</option>
                 </select>
               </div>
+              {errors.category && <p className="text-red-600">{errors.category.message}</p>}
+
               <div className="form-control w-full ">
                 <label className="label">
                   <span className="label-text font-bold">Price</span>
                 </label>
                 <input
-                  {...register('price')}
+                  {...register('price', { required: 'Price is required' })}
                   type="text"
                   placeholder="Price"
                   className="input input-bordered w-full"
                 />
+                {errors.price && <p className="text-red-600">{errors.price.message}</p>}
               </div>
             </div>
+            <div className="flex justify-end"></div>
+
             <div className="form-control">
               <label className="label">
                 <span className="label-text font-bold">Recipe Details</span>
               </label>
               <textarea
-                {...register('recipeDetails')}
+                {...register('recipeDetails', { required: 'Recipe Details is required' })}
                 className="textarea textarea-bordered h-24"
                 placeholder="Recipe Details"></textarea>
             </div>
-            <div className="mt-2">
-              <input type="file" className="file-input w-full max-w-xs" />
+            {errors.recipeDetails && <p className="text-red-600">{errors.recipeDetails.message}</p>}
+            <div className="form-control w-full max-w-xs mt-3">
+              <label className="label">
+                <span className="label-text font-bold">Item Image</span>
+              </label>
+              <input
+                {...register('itemFile', { required: 'Item Image is required' })}
+                type="file"
+                className="file-input w-full max-w-xs"
+              />
             </div>
+            {errors.itemFile && <p className="text-red-600">{errors.itemFile.message}</p>}
 
             <input
               className="mt-4 text-white btn btn-sm bg-orange-400 hover:bg-orange-400"
