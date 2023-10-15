87e28ba4afef7978828c458cc6ecc95fa4c84fcf
Noob-H4cker <alsamiul123@gmail.com>
2023-10-15 11:55:32 +0600

    stripe paymen publishable key added

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -1,11 +1,21 @@
+/* eslint-disable no-undef */
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PB);
+console.log(process.env.REACT_APP_STRIPE_PB);
 
 const Payment = () => {
   return (
     <div>
       <SectionTitle heading={`Payment`} subHeading={`Please Process`}></SectionTitle>
       <h1>This is Payment</h1>
+      <Elements stripe={stripePromise}>
+        <CheckoutForm />
+      </Elements>
     </div>
   );
 };
