0af5e6431377705da39a9c1a962ea638619848d1
Noob-H4cker <alsamiul123@gmail.com>
2023-10-19 14:28:19 +0600

    admin stats data loaded

--- src/Pages/Dashboard/AdminHome/AdminHome.js
+++ src/Pages/Dashboard/AdminHome/AdminHome.js
@@ -1,6 +1,15 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const AdminHome = () => {
+  const { data: adminStats = [] } = useQuery({
+    queryKey: ['admin-stats'],
+    queryFn: async () => {
+      const res = await fetch(`http://localhost:5000/admin-stats`);
+      return res.json();
+    }
+  });
+  console.log(adminStats);
   return (
     <div>
       <h1>This is Admin Home</h1>
